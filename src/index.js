afd81f9c5d7e166f97f5e7514852a34429ea7e3d
Margarita Malakhova <ritulek@mail.ru>
2020-10-25 15:21:57 +0300

    feat: implement function that decodes Morse code

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let numLetters = [];
+    for (let i =0, str = ''; i<expr.length; i++) {
+		str += expr.charAt(i);
+        if ((i+1) % 10 ==0) {
+            numLetters.push(str)
+            str = '';
+        }
+    }
+    let decodedString = '';
+    for (let i =0; i<numLetters.length; i++) {
+		let numLetter = numLetters[i];
+		if (numLetter === '**********') {
+            decodedString += ' ';
+        } else {
+			let codeLetter='';
+			for (let j = 0; j <= numLetter.length; j = j + 2) {
+					if (numLetter[j] === '1' && numLetter[j + 1] === '1') {
+						codeLetter += '-';
+					}
+					if (numLetter[j] === '1' && numLetter[j + 1] === '0') {
+						codeLetter += '.';
+					}
+			}
+			decodedString += MORSE_TABLE[codeLetter];
+		}
+	}
+	return decodedString;
 }
 
 module.exports = {
